50fe911ee8c427c6c5b86eb1d168d92042f01790
furiousnur <furiousnur@gmail.com>
2020-10-09 13:04:18 +0600

    route dynamic and spatie activity log package use

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, LogsActivity;
 
     /**
      * The attributes that are mass assignable.
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return \Spatie\Activitylog\Models\Activity::all();
     return view('welcome');
 });
 
@@ -23,6 +24,7 @@ Route::get('contact',function (){
 });
 
 Route::get('users-list',function (){
-   return view('usersList');
+    $usersList = 'usersList';
+   return view($usersList);
 });
 
